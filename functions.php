f5e1c7e61821d4986fc03266d44a21f6667a8702
johary_git <joharyandria20@gmail.com>
2026-06-10 09:12:05 +0300

    manomboka

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php 
+function dbconnect()
+{
+    static $connect = null;
+    if ($connect === null) {
+        $connect = mysqli_connect('localhost', 'root', '', 'employees');
+        if (!$connect) {
+            die('erreur de connexion a la base de donnee : ' . mysqli_connect_error());
+        }
+        mysqli_set_charset($connect, 'utf8mb4');
+    }
+    return $connect;
+}
+
+
+?>
